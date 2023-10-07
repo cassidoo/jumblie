87dfec45a169e1c90e26895750edca403d487872
Franco Gil <f97gp1@gmail.com>
2023-10-07 10:16:31 -0400

    Introduce style guide and type hints on `wordlist.py` "Python" file

--- words/wordlist.py
+++ words/wordlist.py
@@ -1,17 +1,20 @@
 import json
+from typing import List
 import random
 
-def read_words(filename):
+
+def read_words(filename: str) -> str:
     with open(filename, 'r') as file:
         return file.read().splitlines()
 
-def create_game_data():
-    words_4 = read_words('4words.txt')
-    words_5 = read_words('5words.txt')
-    words_6 = read_words('6words.txt')
-    words_7 = read_words('7words.txt')
 
-    game_data = []
+def create_game_data() -> List[List[str]]:
+    words_4: str = read_words('4words.txt')
+    words_5: str = read_words('5words.txt')
+    words_6: str = read_words('6words.txt')
+    words_7: str = read_words('7words.txt')
+
+    game_data: List[List[str]] = []
 
     for _ in range(min(len(words_4), len(words_5), len(words_6), len(words_7))):
         game_data.append([
@@ -23,13 +26,16 @@ def create_game_data():
 
     return game_data
 
-def write_json(data, filename):
+
+def write_json(data: str, filename: str) -> None:
     with open(filename, 'w') as file:
         json.dump(data, file, indent=4)
 
-def main():
-    game_data = create_game_data()
+
+def main() -> None:
+    game_data: List[List[str]] = create_game_data()
     write_json(game_data, 'wordlist.json')
+
 
 if __name__ == '__main__':
     main()
