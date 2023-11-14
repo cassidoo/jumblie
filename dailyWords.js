91f3fdedf2d0a02b2318bbec0b956f15606bd57c
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2023-11-14 00:19:32 -0600

    Will the time zone issues ever end

--- dailyWords.js
+++ dailyWords.js
@@ -2,15 +2,21 @@
 const startDate = new Date("October 4, 2023");
 
 function getCurrentDate() {
-	let currentDate = new Date();
-	document.getElementById("today").textContent = `${
-		currentDate.getMonth() + 1
-	}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
-	return currentDate;
+	let now = new Date();
+	let localDate = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+	document.getElementById("today").textContent = localDate.toLocaleDateString();
+
+	return localDate;
 }
 
 function getDayDifference() {
-	let timeDifference = getCurrentDate() - startDate;
+	let currentDate = getCurrentDate();
+	let adjustedStartDate = new Date(
+		startDate.getFullYear(),
+		startDate.getMonth(),
+		startDate.getDate()
+	);
+	let timeDifference = currentDate - adjustedStartDate;
 	return Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 }
 
