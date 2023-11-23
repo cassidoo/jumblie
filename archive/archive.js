60463b97efce4335dceff1a6204ef2db49b6d480
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2023-11-22 20:28:55 -0600

    Change archive to use details

--- archive/archive.js
+++ archive/archive.js
@@ -1,37 +1,64 @@
 (function () {
 	addWordsToArchive();
+	if (!navigator.share) {
+		document.getElementById("jshare").remove();
+	}
 })();
 
 function addWordsToArchive() {
 	let dayDifference = getDayDifference();
-	let archive = document.getElementById("archive");
+	let archive = document.getElementById("archivecontainer");
 
 	for (let i = dayDifference - 1; i >= 0; i--) {
-		let row = document.createElement("tr");
+		let row = document.createElement("details");
 		let puzzleNumber = i + 1;
 		let theme = wordList[i % wordList.length].theme;
 		let words = wordList[i % wordList.length].words;
 
-		let puzzleNumberCell = document.createElement("th");
-		puzzleNumberCell.classList.add("num");
-		puzzleNumberCell.textContent = puzzleNumber;
-		row.appendChild(puzzleNumberCell);
+		let puzzleSummary = document.createElement("summary");
+		puzzleSummary.classList.add("puzzle-number");
+		puzzleSummary.innerHTML = `#${puzzleNumber} ${coloredBullets()} ${theme}`;
+		row.appendChild(puzzleSummary);
 
-		let themeCell = document.createElement("td");
-		themeCell.textContent = theme;
-		row.appendChild(themeCell);
+		let wordListCell = document.createElement("ul");
+		wordListCell.classList.add("word-result");
 
 		for (let j = 0; j < words.length; j++) {
-			let wordCell = document.createElement("td");
+			let wordCell = document.createElement("li");
+			wordCell.classList.add(`word-${j}`);
 			wordCell.textContent = words[j];
-			row.appendChild(wordCell);
+			wordListCell.appendChild(wordCell);
 		}
 
+		row.appendChild(wordListCell);
 		archive.appendChild(row);
 	}
+}
+
+function coloredBullets() {
+	let bullets = "";
+	for (let i = 0; i < 4; i++) {
+		bullets += `<span class="color-${i}">•</span>`;
+	}
+	return bullets;
 }
 
 function playJumblie() {
 	window.location.href =
 		"https://jumblie.com/?utm_campaign=archive&utm_source=archive";
+}
+
+function shareLink() {
+	if (navigator.share) {
+		navigator
+			.share({
+				title: "Jumblie",
+				text: "Check out Jumblie, a fun word search game!",
+				url: "https://jumblie.com",
+			})
+			.then(() => console.log("Successful share"))
+			.catch((error) => console.log("Error sharing", error));
+	} else {
+		console.log("Share not supported");
+	}
 }
